4887a9fd689b77a832b3c8d9dc08a56f89cddb23
ae9is <125031666+ae9is@users.noreply.github.com>
2023-04-30 05:38:47 -0700

    feat: add utility functions for parsing dates

--- src/time.ts
+++ src/time.ts
@@ -1,19 +1,47 @@
 // Utility functions
-import { Page } from 'puppeteer-core'
+import { Page } from 'puppeteer-extra-plugin/dist/puppeteer'
 import Logger from './logger'
+import dayjs from 'dayjs'
 
-function delay(time: number) {
+export function delay(time: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, time)
   })
 }
 
-async function longWait(page: Page) {
+export async function longWait(page: Page) {
   Logger.log('Waiting 2000ms ...')
   await page.waitForTimeout(2000)
 }
 
-async function smallWait(page: Page) {
+export async function shortWait(page: Page) {
   Logger.log('Waiting 100ms ...')
   await page.waitForTimeout(100)
+}
+
+export function parseISO8601String(datetime: string) {
+  const parsed = dayjs(datetime)
+  return parsed
+}
+
+// Returns current date in ISO8601, without fraction seconds e.g. '2020-04-02T08:02:17-05:00'
+// ref: https://day.js.org/docs/en/display/format
+export function toISO8601String(datetime: string, format = 'ddd, MMM D YYYY h:mm A') {
+  const parsed = dayjs(datetime, format)
+  const formatted = parsed.format()
+  return formatted
+}
+
+// Formats date string in arbitrary time zone like Date.toISOString().
+// Note: Doesn't work in Node.js. OK in browser.
+// ref: https://stackoverflow.com/questions/49330139
+export function toLocalISOString(datetime: string) {
+  Logger.log(datetime)
+  const date = new Date(datetime)
+  Logger.log(date.toString())
+  Logger.log('' + date.getTime())
+  Logger.log('' + date.getTimezoneOffset()*60000)
+  const localDate = new Date(date.getTime() - date.getTimezoneOffset()*60000)
+  Logger.log('localDate: ' + localDate)
+  return localDate.toISOString().slice(0, -1)
 }
